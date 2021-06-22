4138bccf1ab85b50c374900847a9606f107ea9aa
pwablito <pwablito@gmail.com>
2021-06-22 08:19:07 -0700

    Initial Commit

---
+++ Shared/Code_RecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_RecognitionApp.swift
+//  Shared
+//
+//  Created by Paul Spencer on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Code_RecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
